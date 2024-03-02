80681271f624d84dfed2896fa3f1121c4f045030
NikitaYanakiev <yanakiiev@gmail.com>
2024-03-02 17:06:31 +0100

    homework 20

---
+++ homework_20/js/app.js
@@ -0,0 +1,19 @@
+window.addEventListener('DOMContentLoaded', showCategories);
+
+document.getElementById('categories').addEventListener('click', event => {
+    const categoryId = event.target.getAttribute('data-category');
+    showProductsByCategory(categoryId);
+});
+
+document.getElementById('products').addEventListener('click', event => {
+    const productId = event.target.getAttribute('data-product');
+    const categoryId = event.target.getAttribute('data-category');
+
+    showProductInfo(categoryId, productId);
+});
+
+document.getElementById('product').addEventListener('click', event => {
+    if (event.target.classList.contains('button')) {
+        showShopInformation();
+    }
+});
