bf7de03140e96ad52c7b348d432dc491be6ef22e
NikitaYanakiev <yanakiiev@gmail.com>
2024-04-07 21:17:59 +0200

    homework 28

---
+++ homework_28/script.js
@@ -0,0 +1,44 @@
+function showNextSlide() {
+    const sliderItems = document.querySelectorAll('.slider__item');
+    let nextSlide = document.querySelector('.slider__item.active').nextElementSibling;
+
+    sliderItems.forEach(item => {
+        item.classList.remove('active');
+    });
+
+    if (!nextSlide) {
+        nextSlide = sliderItems[0];
+    }
+
+    nextSlide.classList.add('active');
+
+    clearInterval(sliderInterval);
+    sliderInterval = setInterval(showNextSlide, 3000);
+}
+
+function showPrevSlide() {
+    const sliderItems = document.querySelectorAll('.slider__item');
+    let prevSlide = document.querySelector('.slider__item.active').previousElementSibling;
+
+    sliderItems.forEach(item => {
+        item.classList.remove('active');
+    });
+
+    if (!prevSlide) {
+        prevSlide = sliderItems[sliderItems.length - 1];
+    }
+
+    prevSlide.classList.add('active');
+    clearInterval(sliderInterval);
+    sliderInterval = setInterval(showPrevSlide, 3000);
+}
+
+let sliderInterval = setInterval(showNextSlide, 3000);
+
+document.querySelector('.btns').addEventListener('click', event => {
+    if (event.target.classList.contains('next-btn')) {
+        showNextSlide();
+    } else if (event.target.classList.contains('prev-btn')) {
+        showPrevSlide();
+    }
+});
