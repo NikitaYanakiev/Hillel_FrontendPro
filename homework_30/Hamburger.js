4a20a94f204ea46f9d93cf5bb9fa4ea278e06e6a
NikitaYanakiev <yanakiiev@gmail.com>
2024-04-22 18:22:33 +0200

    homework 30

--- homework_30/Hamburger.js
+++ homework_30/Hamburger.js
@@ -1,5 +1,5 @@
 class Hamburger {
-    
+
     /**
      * 
      * @param {Size{}} size - size of hamburger
@@ -11,13 +11,34 @@ class Hamburger {
         this.toppings = [];
     }
 
-    static SIZE_SMALL = { price: 50, calories: 20};
-    static SIZE_LARGE = { price: 100, calories: 40};
-    static STUFFING_CHEESE = { price: 10, calories: 20};
-    static STUFFING_SALAD = { price: 20, calories: 5};
-    static STUFFING_POTATOES = { price: 15, calories: 10};
-    static TOPPING_SAUCE = { price: 15, calories: 0};
-    static TOPPING_MAYO = { price: 20, calories: 5};
+    static SIZE_SMALL = {
+        price: 50,
+        calories: 20
+    };
+    static SIZE_LARGE = {
+        price: 100,
+        calories: 40
+    };
+    static STUFFING_CHEESE = {
+        price: 10,
+        calories: 20
+    };
+    static STUFFING_SALAD = {
+        price: 20,
+        calories: 5
+    };
+    static STUFFING_POTATOES = {
+        price: 15,
+        calories: 10
+    };
+    static TOPPING_SAUCE = {
+        price: 15,
+        calories: 0
+    };
+    static TOPPING_MAYO = {
+        price: 20,
+        calories: 5
+    };
 
     addTopping(topping) {
         this.toppings.push(topping);
